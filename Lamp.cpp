70dc7c24ca003df5987c14be0b0abbe654ca2d4c
UnityDo <63395346+UnityDo@users.noreply.github.com>
2021-09-14 19:34:05 +0200

    Update Lamp.cpp

--- Lamp.cpp
+++ Lamp.cpp
@@ -8,7 +8,9 @@ ALamp::ALamp()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-    LampMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Lamp"));
+	IntensityOn= 3000.0f;
+	lumensLamp = LumensType::Bulb450;
+	LampMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Lamp"));
 
 }
 
@@ -16,110 +18,147 @@ ALamp::ALamp()
 void ALamp::BeginPlay()
 {
 	Super::BeginPlay();
-    //Crea el mesh de la lampara
-    
-   
-    
 
-    Init();
+    //Configura el timeline
+    if (OnCurve)
+    {
+        FOnTimelineFloat TimelineCallback;
+        FOnTimelineEventStatic TimelineFinishedCallback;
+
+        TimelineCallback.BindUFunction(this, FName("ExecuteTimeline"));
+        TimelineFinishedCallback.BindUFunction(this, FName("OnFinishTimline"));
+
+        MyTimeline = NewObject<UTimelineComponent>(this, FName("OnAnimation"));
+        MyTimeline->AddInterpFloat(OnCurve, TimelineCallback);
+        MyTimeline->SetTimelineFinishedFunc(TimelineFinishedCallback);
+        MyTimeline->RegisterComponent();
+    }
+  
 }
 
 // Called every frame
 void ALamp::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-   
 
-   
+    if (MyTimeline != NULL)
+    {
+        MyTimeline->TickComponent(DeltaTime, ELevelTick::LEVELTICK_TimeOnly, NULL);
+    }
+
 }
 void ALamp::LightOn() {
-
-    FOnTimelineFloat progressFunction{};
-    int index = 0;
     for (ULightComponent* TemporalLight : LightsLamp)
     {
-        TemporalLight->SetIntensity(InicialIntesitys[index]);
-        index++;
-    }
-    if (InstanceMaterial != nullptr)
-    {
-        InstanceMaterial->SetScalarParameterValue(emissionParamName, emissionIntensity);
-        LampMesh->SetMaterial(IndexOfMaterial, InstanceMaterial);
-    }
-    else {
-        UE_LOG(LogTemp, Warning, L"null");
+        TemporalLight->SetIntensity(GetLumenByType());
     }
 
-    //Enciden basado en curve
-   // CurveOn->GetFloatValue(DeltaSeconds)
-   /*
-    progressFunction.BindUFunction(this, "InterpolateIntensity");
-    MyTimline.AddInterpFloat(CurveOn, progressFunction, FName{ TEXT("BlendIntensity") });
-    MyTimline.PlayFromStart();*/
-}
-void ALamp::InterpolateIntensity(float FValue)
-{
-    float StartIntesity = 0.0f;
-    float FinalIntesity = 1.0f;
-    float LastIntesity = 0.0f;
+       
+      
+        DynMaterial->SetScalarParameterValue(ParamEmissionName, EmissionIntesity);
+        DynMaterial->SetScalarParameterValue(ParamOpacity, OpacityIntesity);
     
-    LastIntesity = FMath::Lerp(StartIntesity, FinalIntesity, FValue);
-    if (InstanceMaterial != nullptr)
-    {
-        InstanceMaterial->SetScalarParameterValue(emissionParamName, LastIntesity);
-        LampMesh->SetMaterial(IndexOfMaterial, InstanceMaterial);
-    }
-    else {
-        UE_LOG(LogTemp, Warning, L"null");
-    }
-
-  
+        UE_LOG(LogTemp, Warning, TEXT("[%s] create Material name : %s emision %f"), *this->GetName(), *LampMesh->GetMaterial(0)->GetName(), EmissionIntesity);
+    
+        
 }
-
 void ALamp::LightOff() {
 
     for (ULightComponent* TemporalLight : LightsLamp)
     {
         TemporalLight->SetIntensity(0.0f);
     }
-    if (InstanceMaterial != nullptr)
-    {
-        InstanceMaterial->SetScalarParameterValue(emissionParamName, 0);
-        LampMesh->SetMaterial(IndexOfMaterial, InstanceMaterial);
-    }
-    else {
-        UE_LOG(LogTemp, Warning, L"null");
-    }
- 
+    
+    DynMaterial->SetScalarParameterValue(ParamEmissionName, 0.0f);
+    DynMaterial->SetScalarParameterValue(ParamOpacity, 0.0f);
+
 }
 void ALamp::Flirck() {
 
+    //MyTimeline->PlayFromStart();
+    MyTimeline->SetLooping(true);
+    MyTimeline->Play();
+
 }
-void ALamp::Init() {
-    //Configuracion inicial
-    //Almacena todas las luces
-    //Almacena sus valores inciales de intesity
+
+float ALamp::GetLumenByType()
+{
+    float intesity = 0;
+    switch (lumensLamp)
+    {
+    case LumensType::NoLumen:
+        intesity = 0;
+        break;
+    case LumensType::Bulb450:
+        intesity = 450;
+        break;
+    case LumensType::Bulb800:
+        intesity = 800;
+        break; 
+    case LumensType::Bulb1100:
+            intesity = 1100;
+            break;
+    case LumensType::Bulb1600:
+                intesity = 1600;
+                break;
+    case LumensType::Bulb2600:
+        intesity = 2600;
+        break;
+
+    case LumensType::Bulb5800:
+        intesity = 5800;
+        break;
+
+
+
+    default: break;
+    }
+
+
+	return intesity;
+}
+
+void ALamp::ConfigurationLamp(float Emission, float Opacity)
+{
+    EmissionIntesity = Emission;
+    OpacityIntesity = Opacity;
+    DynMaterial = UMaterialInstanceDynamic::Create(LampMesh->GetMaterial(indexOfMaterial), this);
+    LampMesh->SetMaterial(indexOfMaterial, DynMaterial);
+
+    //Mete los lights en el array, no se puede obtener directamente 
+    TArray<UActorComponent*> temporalArray= this-> GetComponentsByClass(ULightComponent::StaticClass());
    
- 
- TArray<UActorComponent*> TemporalComponents;
- this->GetComponents(ULightComponent::StaticClass(), TemporalComponents, false);
 
-        for (UActorComponent* TemporalLight : TemporalComponents)
-            {
-            ULightComponent* light = Cast<ULightComponent>(TemporalLight);
-             LightsLamp.Add(light);
-             InicialIntesitys.Add(light->Intensity);
-             UE_LOG(LogTemp, Log, TEXT("Found UObject named: %s"), *light->GetName());
-         }
+    for (UActorComponent* component : temporalArray)
+    {
+        ULightComponent* lightComp = Cast<ULightComponent>(component);
+        check(lightComp != nullptr);
+        LightsLamp.Add(lightComp);
+    }
 
-        //Crea copia del material con emission    
-        if (LampMesh != nullptr) {
-          
-            InstanceMaterial=  UMaterialInstanceDynamic::Create(LampMesh->GetMaterial(IndexOfMaterial), this);
-           
-        }
-        else {
-            UE_LOG(LogTemp, Warning, L"null");
-        }
 }
+
+void ALamp::ExecuteTimeline()
+{
+
+    TimelineValue = MyTimeline->GetPlaybackPosition();
+    CurveValue= OnCurve->GetFloatValue(TimelineValue);
+    UE_LOG(LogTemp, Warning, TEXT("Text %f"), CurveValue);
+
+    for (ULightComponent* TemporalLight : LightsLamp)
+    {
+        TemporalLight->SetIntensity(CurveValue *100);
+    }
+
+    DynMaterial->SetScalarParameterValue(ParamEmissionName, CurveValue *100);
+    DynMaterial->SetScalarParameterValue(ParamOpacity, 1);
+
+
+}
+
+void ALamp::OnFinishTimline()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Acabado"));
+}
+
 

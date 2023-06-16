3877b27a619b2d7b31f6494a267a69c767be6457
UnityDo <63395346+UnityDo@users.noreply.github.com>
2023-06-16 09:28:32 +0200

    Update Create_Character.py

--- Create_Character.py
+++ Create_Character.py
@@ -8,7 +8,7 @@ blueprint_factory.set_editor_property("parent_class", unreal.Character)
 
 # Establecer el nombre y la ruta del Blueprint
 blueprint_name = "Personaje1"
-blueprint_path = "/Game/Blueprints"
+blueprint_path = "/Game/Blueprints/NPC"
 
 # Crear el Blueprint
 new_blueprint = unreal.AssetToolsHelpers.get_asset_tools().create_asset(blueprint_name, blueprint_path, unreal.Blueprint, blueprint_factory)
@@ -19,3 +19,11 @@ new_blueprint = unreal.AssetToolsHelpers.get_asset_tools().create_asset(blueprin
 
 blueprint_factory_controller = unreal.BlueprintFactory()
 blueprint_factory_controller.set_editor_property("parent_class", unreal.AIController)
+
+# Establecer el nombre y la ruta del Blueprint
+blueprint_name_controller = "AI_Personaje1"
+blueprint_path_controller = "/Game/Blueprints/NPC"
+
+# Crear el Blueprint
+new_blueprint = unreal.AssetToolsHelpers.get_asset_tools().create_asset(blueprint_name_controller , blueprint_path_controller, unreal.Blueprint, blueprint_factory)
+

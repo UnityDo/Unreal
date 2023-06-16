92b8a2e7a7bcedb5e60de087247ee14cc9ed29a4
UnityDo <63395346+UnityDo@users.noreply.github.com>
2023-06-16 09:26:16 +0200

    Update Create_Character.py
    Add ai controller

--- Create_Character.py
+++ Create_Character.py
@@ -1,7 +1,6 @@
 import unreal
 
-# Obtener el paquete de nivel actual
-current_level = unreal.EditorLevelLibrary().get_editor_world().get_current_level()
+# Crear un Personaje Character
 
 # Crear una nueva clase Blueprint
 blueprint_factory = unreal.BlueprintFactory()
@@ -14,5 +13,9 @@ blueprint_path = "/Game/Blueprints"
 # Crear el Blueprint
 new_blueprint = unreal.AssetToolsHelpers.get_asset_tools().create_asset(blueprint_name, blueprint_path, unreal.Blueprint, blueprint_factory)
 
-# Asignar el nuevo Blueprint al paquete de nivel actual
-unreal.EditorLevelLibrary().replace_actor_with_blueprint(current_level, new_blueprint.get_editor_property("generated_class"), unreal.Transform(), unreal.EditorReplaceActorOptions(keep_scale=True))
+#Crea un AI Controller
+
+
+
+blueprint_factory_controller = unreal.BlueprintFactory()
+blueprint_factory_controller.set_editor_property("parent_class", unreal.AIController)

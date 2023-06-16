457f0378dde5c0033a5a992a1e5e7634f2225408
UnityDo <63395346+UnityDo@users.noreply.github.com>
2023-06-16 09:18:46 +0200

    Update automation_capture.py

--- automation_capture.py
+++ automation_capture.py
@@ -1,16 +1,18 @@
+BAD
 import unreal
 
 capture = unreal.AutomatedLevelSequenceCapture()
-capture.level_sequence_asset =capture.level_sequence_asset = "/Game/Cinematics/Takes/MySequence.MySequence"
+level_sequence_path = "/Game/Cinematics/Takes/MySequence.MySequence"
+level_sequence_soft_path = unreal.SoftObjectPath(level_sequence_path)
+capture.level_sequence_asset = level_sequence_soft_path
 capture.shot_name = 'MyShot'
 capture.use_custom_start_frame = True
-capture.custom_start_frame = 10
+capture.custom_start_frame = unreal.FrameNumber(10)  # Crear objeto FrameNumber con valor 10
 capture.use_custom_end_frame = True
-capture.custom_end_frame = 100
+capture.custom_end_frame = unreal.FrameNumber(100)  # Crear objeto FrameNumber con valor 100
 capture.warm_up_frame_count = 10
 capture.write_edit_decision_list = True
-capture.write_subtitles = True
-capture.write_final_image = True
 
 capture.initialize()
 capture.start_capture()
+

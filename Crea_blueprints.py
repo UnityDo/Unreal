17b57172fcd3208651549b88aba3655881f1ea2f
UnityDo <63395346+UnityDo@users.noreply.github.com>
2023-08-09 12:50:22 +0200

    Create Crea_blueprints.py

---
+++ Crea_blueprints.py
@@ -0,0 +1,28 @@
+import unreal
+
+def make_blueprint_small(asset_name):
+    package_path = "/Game/Blueprints/Interactives/Puzzles/Automatons/"
+    factory = unreal.BlueprintFactory()
+    factory.set_editor_property("parent_class", unreal.Actor)
+    
+    # Crear el Blueprint
+    asset_tools = unreal.AssetToolsHelpers.get_asset_tools()
+    blueprint = asset_tools.create_asset(asset_name, package_path, None, factory)
+    
+    # Agregar componentes al Blueprint
+    root_component = blueprint.simple_construct_default_scene_root()    
+    # Guardar el Blueprint
+    unreal.EditorAssetLibrary.save_asset(package_path + "/" + asset_name)
+
+# Nombres de los Blueprints
+blueprint_names = ["Devil", "Justice", "Magician", "Priestess", "Death"]
+
+# Recorrer los nombres y crear los Blueprints
+for name in blueprint_names:
+    # Nombre del Blueprint
+    blueprint_name = f"BP_Body_{name}"
+    
+    # Crear el Blueprint
+    make_blueprint_small(blueprint_name)
+    
+    print(f"Blueprint '{blueprint_name}' creado y guardado en '{package_path}/{blueprint_name}'")
